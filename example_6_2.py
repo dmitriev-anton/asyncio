257a0e868a576a3a810e299453a431af77efacb0
Anton Dmitriev <spaceboobman@gmail.com>
2024-04-25 15:33:06 +0400

    simplest weather reflector

--- example_6_2.py
+++ example_6_2.py
@@ -4,7 +4,7 @@ import asyncio
 import json
 from aiohttp import ClientSession, web
 
-#mirroring weather http://localhost:8080/weather?city=Izhevsk
+# added translation to russian
 async def get_weather(city):
     async with ClientSession() as session:
         url = f'http://api.openweathermap.org/data/2.5/weather'
@@ -18,10 +18,31 @@ async def get_weather(city):
                 return 'Нет данных'
 
 
+async def get_translation(text, source, target):
+    async with ClientSession() as session:
+        url = 'https://libretranslate.com/translate'
+
+        data = {'q': text, 'source': source, 'target': target, 'format': 'text', 'api_key': ""}
+
+        headers = {"Content-Type": "application/json"}
+
+        async with session.post(url, json=data, headers=headers, ssl=False) as response:
+            translate_json = await response.json()
+
+            try:
+                return translate_json['translatedText']
+            except KeyError:
+                return text
+
+
 async def handle(request):
-    city = request.rel_url.query['city']
-    weather = await get_weather(city)
-    result = {'city': city, 'weather': weather}
+    city_ru = request.rel_url.query['city']
+    city_en = await get_translation(city_ru, 'ru', 'en')
+
+    weather_en = await get_weather(city_en)
+    weather_ru = await get_translation(weather_en, 'en', 'ru')
+
+    result = {'city': city_ru, 'weather': weather_ru}
 
     return web.Response(text=json.dumps(result, ensure_ascii=False))
 
